8728bbbe9b629f48b6b83fd901c7f974866cb6cc
Vincent Berenz <vberenz@tuebingen.mpg.de>
2022-09-18 16:04:18 +0200

    dev

--- camera_zwo_asi/picture_matrix.py
+++ camera_zwo_asi/picture_matrix.py
@@ -19,14 +19,6 @@ class ParameterRange:
         """
         return list(range(self.min, self.max, self.step))
 
-    def next(self)->int:
-        """
-        function that yield the next value of the range
-        """
-        for value in range(self.min, self.max, self.step):
-            yield value
-        return None
-
 
 def iterate_parameters(
     parameters: typing.List[ParameterRange]
@@ -41,53 +33,90 @@ def iterate_parameters(
     return None
 
 
+def set_control(
+        camera: Camera,
+        parameter: str,
+        value: int,
+        timeout: float,
+        threshold: float
+)->None:
+    camera.set_control(parameter,value)
+    start = time.time()
+    while time.time()-start < timeout:
+        obtained_value = camera.get_controls()[parameter].value
+        if abs(value-obtained_value)<threshold:
+            return
+        time.sleep(0.01)
+    
+
 def add_to_hdf5(
-    camera: Camera,
-    parameters: typing.Dict[str,int],
-    hdf5_file: h5py.File,
-    avg_over: int
+        camera: Camera,
+        parameters: typing.Dict[str,int],
+        hdf5_file: h5py.File,
+        avg_over: int,
+        previous_parameters: typing.Optional[typing.Dict[str,int]]=None
 )->None:
 
+    # setting the configuration
+    for index, (parameter, value) in enumerate(parameters.items()):
+        if previous_parameter is not None and previous_parameter[parameter]!=value:
+            camera.set_control(parameter, value)
+
+    # getting the configuration values, which may or may not
+    # be what we asked for
+    all_parameters = camera.get_controls() 
+    current_parameters = OrderedDict()
+    for parameter in parameters.keys():
+        current_parameters[key] = all_parameters[parameter].value
+    
     # taking and averaging the pictures
-    for parameter, value in parameters.items():
-       camera.set_control(parameter, value)
     images = [camera.capture() for _ in range(avg_over)]
     image = np.mean(images, axis=0)
 
     # adding the image to the hdf5 file
     group = hdf5_file
-    for parameter, value in parameters.items():
+    for parameter, value in current_parameters.items():
         group = hdf5_file.require_group(str(value))
     group.create_dataset("image", data=image.get_data())
 
+    # add the camera current configuration to the group
+    group.attrs["camera_config"] = camera.to_toml()
 
-def get_image_matrix(
+
+def create_hdf5_library(
     camera: Camera, 
     parameters: typing.OrderedDict[str,ParameterRange],
     config: typing.Optional[typing.Union[Path,typing.Mapping[str,typing.Any]]],
+    avg_over: int,
     hdf5_path: Path
 )->:
 
     # opening the hdf5 file in write mode
-    hdf5_file = h5py.File(hdf5_path, "w")
+    with h5py.File(hdf5_path, "w") as hdf5_file:
     
-    # configuring the camera
-    if config is not None:
-        camera.configure_from_toml(config)
+        # configuring the camera
+        if config is not None:
+            camera.configure_from_toml(config)
 
-    # getting the image size
-    roi = camera.get_roi()
-    image_type = roi.type
-    image_class = get_image_class(type)
-    image = image_class(roi.width, roi.height)
-    image_size = image.get_data_size()
+        # adding the parameters to the hd5 file
+        data = repr(params)
+        hdf5_file.attrs["parameters"] = data
 
-    # iterating over all the parameters and adding
-    # the images to the hdf5 file
-    for values in iterate_parameters(parameters.values()):
-        parameters = OrderedDict(zip(parameters.keys(), values))
-        add_to_hdf5(camera, parameters, hdf5_file, avg_over=1)
+        # iterating over all the parameters and adding
+        # the images to the hdf5 file
+        for values in iterate_parameters(params.values()):
+            add_to_hdf5(
+                camera, values,
+                hdf5_file, avg_over=avg_over
+            )
         
-    # closing the hdf5 file
-    hdf5_file.close()
     
+class ImageLibrary:
+
+    def __init__(
+            self,
+            hdf5_path: Path
+    ):
+        self._hdf5_file = h5py.File(hdf5_path,"r")
+
+        
